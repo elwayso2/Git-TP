32c278d085d8ece75ddf0fbcaa27ed80d84ab3e6
elwayso2 <2560500@etu.cegep-lanaudiere.qc.ca>
2025-09-21 15:45:41 -0400

    erreur menu

--- TP1.py
+++ TP1.py
@@ -7,23 +7,37 @@ text_normal: str = "\033[0m"
 
 
 # variable
-mode_Choisi: int
+mode_Choisi: int = 0
 nombre_random: int
 nombre_guess: int
-nombre_maximum: int
+nombre_maximum: int = 0
 nombre_minimum: int = 1
-essai_actuel: int = 1
-essai_Maximum: int
-nombre_random = random.randint(1, 20)
-i: int
+essai_actuel: int = 0
+essai_Maximum: int = 0
+nombre_random = 0
+variable_De_Controle : bool = False
 
-print(text_normal + "Choisir une des options suivantes (entrez un nombre entre 1 et 4): ")
+print(text_normal + "Choisir une des options suivantes (entrez un chiffre entre 1 et 4): ")
 print("1 - Facile: Nombre entre 1 et 19, 10 essais")
 print("2 - Normal: Nombre entre 1 et 49, 5 essais")
 print("3 - Diffile: Nombre entre 1 et 99, 5 essais")
 print("4 - Impossible: Nombre entre 1 et 999, 1 essai")
 
-mode_Choisi = int(input(""))
+while not variable_De_Controle:
+
+    try:
+        mode_Choisi = int(input(""))
+        if 0 < mode_Choisi <= 4:
+            variable_De_Controle = True
+        else:
+            print("veuillez entrer un chiffre entre 1 et 4")
+
+    except ValueError:
+        print("veuillez entrer un chiffre valide")
+        
+
+
+
 
 
 
